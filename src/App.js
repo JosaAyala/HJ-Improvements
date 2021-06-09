b3ecba4247d6d734c2840539a3a419968cb21cb0
JosaAyala <kensy.ayala1994@gmail.com>
2021-06-08 20:22:26 -0600

    change icon and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import { FooterComponent } from "./components/Footer/Footer";
 import MainScreenComponent from "./components/MainScreen";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreenComponent from "./components/HomeScreen";
+import ContactScreenComponent from "./components/ContactScreen";
+import GalleryScreenComponent from "./components/GalleryScreen";
+import ServicesScreenComponent from "./components/ServicesScreen";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MainScreenComponent />
+        <Route exact path="/" component={HomeScreenComponent} />
+        <Route exact path="/home" component={HomeScreenComponent} />
+        <Route exact path="/services" component={ServicesScreenComponent} />
+        <Route exact path="/gallery" component={GalleryScreenComponent} />
+        <Route exact path="/contact" component={ContactScreenComponent} />
         <FooterComponent />
       </BrowserRouter>{" "}
     </div>
