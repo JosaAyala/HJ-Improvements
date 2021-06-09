2a887b80fcca6d76c272ddaf19f626d68ebba349
JosaAyala <kensy.ayala1994@gmail.com>
2021-06-08 21:01:01 -0600

    fixed issue with router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { FooterComponent } from "./components/Footer/Footer";
 import MainScreenComponent from "./components/MainScreen";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeScreenComponent from "./components/HomeScreen";
 import ContactScreenComponent from "./components/ContactScreen";
 import GalleryScreenComponent from "./components/GalleryScreen";
@@ -10,7 +10,7 @@ import ServicesScreenComponent from "./components/ServicesScreen";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <MainScreenComponent />
         <Route exact path="/" component={HomeScreenComponent} />
         <Route exact path="/home" component={HomeScreenComponent} />
@@ -18,7 +18,7 @@ function App() {
         <Route exact path="/gallery" component={GalleryScreenComponent} />
         <Route exact path="/contact" component={ContactScreenComponent} />
         <FooterComponent />
-      </BrowserRouter>{" "}
+      </Router>
     </div>
   );
 }
